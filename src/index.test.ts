cd8dd58463e5db17e4f95d5c224bc53d3f67b7c6
ScottMorse <scottmorsedev@gmail.com>
2022-09-17 12:19:58 -0500

    Attempt

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,7 @@ import message from "."
 
 describe("Dummy test", () => {
   test("Tests work", () => {
+    throw new Error("Test failure")
     expect(message).toBe("I VVANT TO CREATE AN AUDIO PROCESSING GRAPH")
   })
 })
