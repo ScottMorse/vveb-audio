8c70af82ec140d9eee2d48b4ec05f7e327f1e565
Scott Morse <scottmorsedev@gmail.com>
2022-09-17 13:16:25 -0500

    Update typed event emitter types

--- src/lib/util/events/typedEventEmitter.ts
+++ src/lib/util/events/typedEventEmitter.ts
@@ -3,68 +3,41 @@ import { StringKeys } from "@/lib/util/types"
 
 type BaseEventConfig = { [key: string]: any }
 
-type EventValueArgs<
+type EventArgs<
   E extends keyof EventConfig,
   EventConfig extends BaseEventConfig
 > = EventConfig[E] extends undefined ? [] : [EventConfig[E]]
 
-/**
- * A standard EventEmitter that provides enhanced typing for specific events.
- *
- * The EventConfig generic type should map event names to the value
- * that will be passed to a listener. The event emitter is then required to
- * emit events with the correct value type, and listeners must expect
- * the correct type parameter.
- *
- * @example
- * ```typescript
- * interface MyEvents {
- *   customEventA: string,
- *   customEventB: { foo: 'bar' | 'baz' },
- *   customEventC: undefined,
- * }
- *
- * const emitter = new TypedEventEmitter<MyEvents>()
- *
- * emitter.emit('customEventA', 'something') // verified payload for customEventA
- * emitter.emit('customEventB', { foo: 'bar' }) // verified payload for customEventB
- * emitter.emit('customEventC') // arg not required for customEventC when payload is undefined
- *
- * emitter.on('customEventA', (s: string) => console.log(s)) // verified listener arg for customEventA
- * emitter.on('customEventB', (obj) => console.log(obj.foo)) // verified listener arg for customEventB
- * emitter.on('customEventC', () => console.log('C fired')) // no listener arg for customEventC when payload is undefined
- * ```
- */
-interface TypedEventEmitterInstance<EventConfig extends BaseEventConfig> {
+interface _TypedEventEmitter<EventConfig extends BaseEventConfig> {
   emit<E extends StringKeys<EventConfig>>(
     event: E,
-    ...args: EventValueArgs<E, EventConfig>
+    ...args: EventArgs<E, EventConfig>
   ): boolean
 
   on<E extends StringKeys<EventConfig>>(
     event: E,
-    listener: (...args: EventValueArgs<E, EventConfig>) => void
-  ): TypedEventEmitterInstance<EventConfig>
+    listener: (...args: EventArgs<E, EventConfig>) => void
+  ): _TypedEventEmitter<EventConfig>
 
   off<E extends StringKeys<EventConfig>>(
     event: E,
-    listener: (...args: EventValueArgs<E, EventConfig>) => void
-  ): TypedEventEmitterInstance<EventConfig>
+    listener: (...args: EventArgs<E, EventConfig>) => void
+  ): _TypedEventEmitter<EventConfig>
 
   once<E extends StringKeys<EventConfig>>(
     event: E,
-    listener: (...args: EventValueArgs<E, EventConfig>) => void
-  ): TypedEventEmitterInstance<EventConfig>
+    listener: (...args: EventArgs<E, EventConfig>) => void
+  ): _TypedEventEmitter<EventConfig>
 
   prependListener<E extends StringKeys<EventConfig>>(
     event: E,
-    listener: (...args: EventValueArgs<E, EventConfig>) => void
-  ): TypedEventEmitterInstance<EventConfig>
+    listener: (...args: EventArgs<E, EventConfig>) => void
+  ): _TypedEventEmitter<EventConfig>
 
   prependOnceListener<E extends StringKeys<EventConfig>>(
     event: E,
-    listener: (...args: EventValueArgs<E, EventConfig>) => void
-  ): TypedEventEmitterInstance<EventConfig>
+    listener: (...args: EventArgs<E, EventConfig>) => void
+  ): _TypedEventEmitter<EventConfig>
 
   eventNames(): StringKeys<EventConfig>[]
 
@@ -72,12 +45,19 @@ interface TypedEventEmitterInstance<EventConfig extends BaseEventConfig> {
 
   removeAllListeners<E extends StringKeys<EventConfig>>(
     event?: E
-  ): TypedEventEmitterInstance<EventConfig>
+  ): _TypedEventEmitter<EventConfig>
 
   rawListeners<E extends StringKeys<EventConfig>>(event: E): Listener[]
 
   listeners<E extends StringKeys<EventConfig>>(event: E): Listener[]
+
+  addListener: _TypedEventEmitter<EventConfig>["on"]
+  removeListener: _TypedEventEmitter<EventConfig>["off"]
 }
+
+type TypedEventEmitterInstance<EventConfig extends BaseEventConfig> =
+  _TypedEventEmitter<EventConfig> &
+    Omit<EventEmitter, keyof _TypedEventEmitter<EventConfig>>
 
 /**
  * A standard EventEmitter that provides enhanced typing for specific events.
